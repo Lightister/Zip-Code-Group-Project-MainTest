5d8ce8d08327f28605c72d6d9c09819e42a8f610
Lightister <150493681+Lightister@users.noreply.github.com>
2024-10-02 20:25:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * @file main.cpp
+ * @brief Implementation file that runs inherited functions.
+ * 
+ * Implemention of all the classes in this project especially CSVProcessing.h
+ * 
+ * @author 
+ * Aidan Adams
+ * @date 
+ * 10/2/2024
+ */
+ 
+ /**
+ * @brief Generates a header and puts the data into an output file
+ * 
+ * This Function uses the .addHeader() function to generate a premade header. 
+ * The function then follow
+ * 
+ * @param origin the processor struct that is going to be used.
+ * @param file the name to be given to the output file.
+ */
+
 void csvConvert_sort(CSVProcessing origin, string file)
 {
 	cout << "Generating header row." << endl;
